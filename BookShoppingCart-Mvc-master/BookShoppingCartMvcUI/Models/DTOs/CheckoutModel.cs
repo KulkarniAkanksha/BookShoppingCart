144f75fa5cf777be8e3994d874e3e6c8c8e1d7c9
KulkarniAkanksha <121750676+KulkarniAkanksha@users.noreply.github.com>
2024-07-12 19:06:58 +0530

    BookShoppingCart

---
+++ BookShoppingCart-Mvc-master/BookShoppingCartMvcUI/Models/DTOs/CheckoutModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShoppingCartMvcUI.Models.DTOs
+{
+    public class CheckoutModel
+    {
+        [Required]
+        [MaxLength(30)]
+        public string? Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(30)]
+        public string? Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[6-9]\d{9}$", ErrorMessage = "Mobile number should be 10 digits and start with 6-9.")]
+        public string? MobileNumber { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string? Address { get; set; }
+
+        [Required]
+        public string? PaymentMethod { get; set; }
+    }
+}
